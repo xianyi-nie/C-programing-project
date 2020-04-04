7b3a8013c935d191b3b8cfde3d66083f9f7280f1
Coursera Learner <nobody@nowhere.nul>
2020-04-04 14:18:53 +0000

    Did assignment03 changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit.  # On branch master # Your branch is up-to-date with 'origin/master'.  # # Changes to be committed: # modified: code2.c #

--- 03_code2/code2.c
+++ 03_code2/code2.c
@@ -1,23 +1,16 @@
 
 int printTriangle(int size) {
-  //start with starCount being 0
-
-  //count from 0 (inclusive) to size (exclusive), for each number i that you count
-
-     //count from 0 (inclusive) to i (inclusive), for each number j that you count
-
-        //print a "*"
-
-        //increment starCount
-
-     //when you finish counting on j, 
-
-     //print a newline ("\n")
-
-  //when you finish counting on i, 
-
-  //your answer is starCount
-
+  int starCount=0; //start with starCount being 0
+  for (int i=0;i<size;i++)  //count from 0 (inclusive) to size (exclusive), for each number i that you count
+    {
+      for (int j=0;j<=i;j++)     //count from 0 (inclusive) to i (inclusive), for each number j that you count
+	{
+	  printf ("*");        //print a "*"
+	  starCount++;        //increment starCount
+	}     //when you finish counting on j, 
+      printf ("\n");     //print a newline ("\n")
+    }  //when you finish counting on i, 
+  return starCount;  //your answer is starCount
 }
 
 
@@ -26,11 +19,9 @@ int main(void) {
   printf("Here is a triangle with height 4\n");
   numStars = printTriangle(4);
   printf("That triangle had %d total stars\n", numStars);
-  //now print "Here is a triangle with height 7\n"
-
-  //then call printTriangle, passing in 7, and assign the result to numStars
-
-  //finally, print "That triangle had %d total stars\n", such that the %d 
+  printf("Here is a triangle with height 7\n");//now print "Here is a triangle with height 7\n"
+  numStars = printTriangle(7);  //then call printTriangle, passing in 7, and assign the result to numStars
+  printf("That triangle had %d total stars\n",numStars); //finally, print "That triangle had %d total stars\n", such that the %d 
   //prints the value of numStars
 
 
