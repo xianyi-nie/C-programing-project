87746bcc1536a844d8e319b315be3268c1e5303a
Coursera Learner <nobody@nowhere.nul>
2020-04-09 07:20:35 +0000

    Did it again

--- 07_retirement/retirement.c
+++ 07_retirement/retirement.c
@@ -30,13 +30,12 @@ void retirement (int starAge, double initial, retire_info working, retire_info r
 
 int main()
 {
-  retire_info working;
-  retire_info retired;
- 
+  retire_info working; 
   working.months = 489;
   working.contribution = 1000;
   working.rate_of_return = 0.045/12;
-    
+
+  retire_info retired;
   retired.months = 384;
   retired.contribution = -4000;
   retired.rate_of_return = 0.01/12;
